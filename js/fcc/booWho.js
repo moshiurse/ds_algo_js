2d03fa12ecc40ccea640697476016221ddbe9d2e
Moshiur Rahman <15215020+moshiurse@users.noreply.github.com>
2019-11-06 06:05:16 +0600

    Create booWho.js

---
+++ js/fcc/booWho.js
@@ -0,0 +1,11 @@
+function booWho(bool) {
+  // What is the new fad diet for ghost developers? The Boolean.
+  if(bool === true || bool === false){
+    return true;
+  }else{
+    return false;
+  }
+  
+}
+
+booWho(null);
