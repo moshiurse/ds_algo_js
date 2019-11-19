5f4a408ba5e069793360406b65fe2cfb699c6ba0
Moshiur Rahman <15215020+moshiurse@users.noreply.github.com>
2019-11-19 17:17:50 +0600

    Create Binary Agents.js

---
+++ js/fcc/Binary Agents.js
@@ -0,0 +1,9 @@
+function binaryAgent(str) {
+  var arr = str.split(' ');
+  arr = arr.map(value => {
+    return String.fromCharCode(parseInt(value,2));
+  })
+  return arr.join('');
+}
+
+binaryAgent("01000001 01110010 01100101 01101110 00100111 01110100 00100000 01100010 01101111 01101110 01100110 01101001 01110010 01100101 01110011 00100000 01100110 01110101 01101110 00100001 00111111");
