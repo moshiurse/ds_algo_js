85fa958bead065f523441726c6ce7ad0ac7dceb9
Moshiur Rahman <rmoshiur705@gmail.com>
2022-04-06 01:27:12 +0600

    941. Valid Mountain Array

---
+++ leetcode/941_ValidMountainArray.js
@@ -0,0 +1,57 @@
+// Given an array of integers arr, return true if and only if it is a valid mountain array.
+
+// Recall that arr is a mountain array if and only if:
+
+// arr.length >= 3
+// There exists some i with 0 < i < arr.length - 1 such that:
+// arr[0] < arr[1] < ... < arr[i - 1] < arr[i]
+// arr[i] > arr[i + 1] > ... > arr[arr.length - 1]
+
+
+
+// Example 1:
+
+// Input: arr = [2,1]
+// Output: false
+// Example 2:
+
+// Input: arr = [3,5,5]
+// Output: false
+// Example 3:
+
+// Input: arr = [0,3,2,1]
+// Output: true
+
+
+// Constraints:
+
+// 1 <= arr.length <= 104
+// 0 <= arr[i] <= 104
+
+var validMountainArray = function (arr) {
+    if (arr.length < 3) return false;
+    let left = true;
+    let right = true;
+    let trackValue = arr[0];
+    for (let i = 1; i < arr.length; i++) {
+
+        if (left && arr[i] > trackValue) {
+            right = false;
+            console.log('if', i, arr[i], trackValue);
+            trackValue = arr[i];
+        } else {
+            left = false;
+            if (arr[i] < trackValue) {
+                trackValue = arr[i];
+            } else {
+                return false;
+            }
+        }
+    }
+
+    if(left || right) return false;
+
+    return true;
+};
+
+console.log(validMountainArray([9,8,7,6,5,4,3,2,1,0]));
