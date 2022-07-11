e7e1fc0f25630f5727ff7c454c073fe72a219a11
Moshiur Rahman <rmoshiur705@gmail.com>
2022-07-11 23:42:07 +0600

    Implement Singly linked list

---
+++ UDMTCIDS&A/LinkedList/linkedList.js
@@ -0,0 +1,90 @@
+class LinkedList {
+    constructor(value) {
+        this.head = {
+            value,
+            next: null
+        };
+        this.tail = this.head;
+        this.length = 1;
+        return this;
+    }
+
+    append(value) {
+        const newNode = {
+            value,
+            next: null
+        }
+        this.tail.next = newNode;
+        this.tail = newNode;
+        this.length++;
+        return this;
+    }
+
+    prepend(value) {
+        let currentNode = this.head;
+        this.head = {
+            value,
+            next: currentNode
+        }
+        this.length++;
+        return this;
+    }
+
+    printList() {
+        let currentNode = this.head;
+        let array = [];
+        while (currentNode != null) {
+            array.push(currentNode.value);
+            currentNode = currentNode.next;
+        }
+
+        console.log(array);
+        return array;
+    }
+
+    insert(index, value) {
+        if(index >= this.length){
+            return this.append(value);
+        }
+
+        const newNode = {
+            value,
+            next: null
+        }
+
+        const leader = this.traverseToIndex(index - 1);
+        const holdingPointer = leader.next;
+        leader.next = newNode;
+        newNode.next = holdingPointer;
+        this.length++;
+        return this.printList();
+    }
+
+    remove(index) {
+        const leader = this.traverseToIndex(index - 1);
+        const unwantedNode = leader.next;
+        leader.next = unwantedNode.next;
+        this.length--;
+        return this.printList();
+    }
+
+    traverseToIndex(index) {
+        let counter = 0;
+        let currentNode = this.head;
+
+        while (counter != index) {
+            currentNode = currentNode.next;
+            counter++;
+        }
+
+        return currentNode;
+    }
+}
+
+let myLink = new LinkedList(1);
+myLink.append(2);
+myLink.append(4);
+myLink.append(6);
+myLink.prepend(10);
+myLink.printList();
+myLink.remove(3);
