ee4738ff2ed4d928e30048eddacebd536f56ce66
Mosiur Rahaman <rmoshiur705@gmail.com>
2023-03-16 11:40:42 +0600

    Added 6-10 pattern of 22 patterns

--- dsa implementation/22paterns.js
+++ dsa implementation/22paterns.js
@@ -54,5 +54,77 @@ const pattern5 = (n) => {
     return str;
 }
 
+const pattern6 = (n) => {
+    let str = '';
+    for (let i = 1; i <= n; i++) {
+        for (let j = 1; j <= n-i; j++) {
+            str += j;
+        }
+        str += '\n';
+    }
+    return str;
+}
 
-console.log(pattern5(6));
+const pattern7 = (n) => {
+    let str = '';
+    for (let i = 0; i < n; i++) {
+        for (let j = 0; j < n-i-1; j++) {
+            str += ' ';
+        }
+        for (let j = 0; j < 2*i+1; j++) {
+            str += '*';
+        }
+        for (let j = 0; j < n-i-1; j++) {
+            str += ' ';
+        }
+        str += '\n';
+    }
+    return str;
+}
+
+const pattern8 = (n) => {
+    let str = '';
+    for (let i = 0; i < n; i++) {
+        for (let j = 0; j < i; j++) {
+            str += ' ';
+        }
+        for (let j = 0; j < (n-i-1)*2 + 1; j++) {
+            str += '*';
+        }
+        for (let j = 0; j < i; j++) {
+            str += ' ';
+        }
+        str += '\n';
+    }
+    return str;
+}
+
+const pattern9 = (n) => {
+    let str = '';
+    str += pattern7(n);
+    str += pattern8(n);
+    return str;
+}
+
+const pattern10 = (n) => {
+    let str = '';
+    for (let i = 1; i <= n; i++) {
+        for (let j = 0; j < i; j++) {
+            str += '*';
+        }
+        str += '\n';
+    }
+    for (let i = n-1; i > 0; i--) {
+        for (let j = 0; j < i; j++) {
+            str += '*';
+        }
+        str += '\n';
+    }
+    return str;
+}
+
+
+
+
+
+console.log(pattern10(6));
