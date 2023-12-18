a16d59c4e3bee24ce8f96c02366bddc4d541d613
Moshiur Rahman <15215020+moshiurse@users.noreply.github.com>
2023-12-18 12:36:04 +0600

    Time: 79 ms (38.15%) | Memory: 43.2 MB (75.47%) - LeetSync

---
+++ leetcode/242-valid-anagram/valid-anagram.js
@@ -0,0 +1,31 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isAnagram = function(s, t) {
+    if(s.length != t.length) return false;
+    let result = true;
+    let map = {};
+
+    for(let i = 0; i < s.length; i++){
+        if(!map[s[i]]){
+            map[s[i]] = 1;
+        }else{
+            map[s[i]] = map[s[i]] + 1;
+        }
+    }
+    console.log(map)
+
+    for(let i = 0; i < t.length; i++){
+        if(map[t[i]]){
+            map[t[i]] = map[t[i]] - 1;
+        }else{
+            result = false;
+            break;
+        }
+    }
+
+    return result;
+
+};
