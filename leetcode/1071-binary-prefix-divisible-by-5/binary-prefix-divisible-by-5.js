39bf5407a72c06feed6571bde61d7e2b5e77df7a
Moshiur Rahman <15215020+moshiurse@users.noreply.github.com>
2023-12-31 12:12:55 +0600

    Time: 47 ms (100.00%) | Memory: 46.4 MB (38.10%) - LeetSync

--- leetcode/1071-binary-prefix-divisible-by-5/binary-prefix-divisible-by-5.js
+++ leetcode/1071-binary-prefix-divisible-by-5/binary-prefix-divisible-by-5.js
@@ -6,8 +6,8 @@ var prefixesDivBy5 = function(nums) {
     let result = [];
     let bin = 0;
     for(let i = 0;i < nums.length; i++){
-        bin = (bin % 5) * 2 + nums[i];
-        result.push(( bin % 5 == 0));
+        bin = (bin  * 2 + nums[i]) % 5;
+        result.push(bin == 0);
     }
     return result
 };
